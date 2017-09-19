7b97ff3ae1ef94705b50e2b787b1ada086342b8c
Paula Crismaru <paulacrismaru@gmail.com>
2017-09-19 12:32:45 +0300

    updated joker

--- joker/README.md
+++ joker/README.md
@@ -1,62 +1,181 @@
 ### problema joker
 http://campion.edu.ro/arhiva/index.php?page=problem&action=view&id=1007
 
-1. Se sorteaza sirul si se calculeaza lungimea celui mai lung subsir crescator
-2. Daca in sir nu avem jokerul, afisam maximul de la 1.
-3. Daca in sir avem jokerul, cream 2 vectori ce vor tine ultimul element al unui subsir (ue) si lungimea subsirului (ls).
-4. Parcurgem ue si verificam daca exista 2 subsiruri consecutive alaturare care pot fi unite cu un joker
-5. Afisam maximul
+1. Se sorteaza sirul
+2. Se parcurge sirul sortat si se calculeaza lungimea sirului curent cu ajutorul unui parametru ce va tine pozitia in care incepe ultimul sir crescator si in acelasi timp calculam si lungimea maxima ce se cere.
+3. Daca in sir avem jokerul
+    a). in momentul in care diferenta a 2 vecini e 2, utilizam jokerul si il marcam ca si folosit
+    b). daca am ajuns la sfarsitul unui sir consecutiv si nu am utilizat jokerul, verificam daca il putem lipi la vreun capat al sirului
+4. Afisam maximul
 
 De exp:
-a = 6, 2, 4, 7, 0
+```
+joker.in
+    10 10
+    5
+    8
+    9
+    10
+    11
+    12
+    13
+    1
+    16
+    4
+    0
+```
+```
+a = [0, 1, 4, 5, 8, 9, 10, 11, 12, 13]
+k = 11
+n = 10
+exista_joker = 1
+```
+### i = 2
+```
+lungime_sir_curent = 1
+joker = 0
+maxim = 1
+```
+         0  1  2  3  4  5   6  7   8   9   
+    a = [0, 1, 3, 5, 8, 9, 10, 11, 12, 13]
+            |  |
+            u  i
+            consecutive? NU
+            jokerul neutilizat &&
+             se portiveste intre 1 si 3 &&
+             n permite (pentru [1, 3])
+                lungime_sir_curent = 3 DA
+                maxim = 3
+                joker = 1
+                i++
 
-a = 0, 2, 4, 6, 7
+### i = 3
+```
+lungime_sir_curent = 3
+joker = 1
+maxim = 3
+```
+         0  1  2  3  4  5   6  7   8   9 
+    a = [0, 1, 3, 5, 8, 9, 10, 11, 12, 13]
+            |     |
+            u     i
+            consecutive? NU
+            jokerul neutilizat NU
+                lungime_sir_curent = 1
+                jocker = 0
+                u = i
+                i++
 
-maxim = 2 (6 si 7)
+### i = 3
+```
+lungime_sir_curent = 1
+joker = 0
+maxim = 3
+```
+         0  1  2  3  4  5   6  7   8   9 
+    a = [0, 1, 3, 5, 8, 9, 10, 11, 12, 13]
+               |  |
+               u  i
+            consecutive? NU
+            jokerul neutilizat &&
+             se portiveste intre 3 si 5 DA
+             n permite (pentru [3, 5]) DA
+                lungime_sir_curent = 3
+                joker = 1
+                i++
 
-1).
+### i = 4
+``` 
+lungime_sir_curent = 3
+joker = 1
+maxim = 3
+```
+         0  1  2  3  4  5   6  7   8   9 
+    a = [0, 1, 3, 5, 8, 9, 10, 11, 12, 13]
+               |     |
+               u     i
+            consecutive? NU
+            jokerul neutilizat NU
+                lungime_sir_curent = 1
+                jocker = 0
+                u = i
+                i++
 
-        ======          
-        ue  ls              
-        ====== 
-        2   1   \
-        4   1   /    
-        7   2      
+### i = 4
+``` 
+lungime_sir_curent = 1
+joker = 0
+maxim = 3
+```
+         0  1  2  3  4  5   6  7   8   9 
+    a = [0, 1, 3, 5, 8, 9, 10, 11, 12, 13]
+                  |  |
+                  u  i
+            consecutive? NU
+            jokerul neutilizat &&
+             se portiveste intre 3 si 5 NU
+                lungime_sir_curent = 1
+                u = i
+                i++
 
-        ======          
-        ue  ls                jokerul
-        ======                 \/ 
-    ->  2   1   \ 4 - 1 == 2 + 1 ?
-    ->  4   1<- /     /\
-        7   2      1 element in subsirul cu capatul 4 (din ls)
+### i = 5
+``` 
+lungime_sir_curent = 1
+joker = 0
+maxim = 3
+```
+         0  1  2  3  4  5   6  7   8   9 
+    a = [0, 1, 3, 5, 8, 9, 10, 11, 12, 13]
+                     |  |
+                     u  i
+            consecutive? DA
+                lungime_sir_curent = 2
+                i++
 
-        ======          
-        ue  ls               
-        ======                
-        2   1<- \ 4 - 1 == 2 + 1 DA
-        4   1<- / 
-        7   2      maxim = 1 + 1 + 1 = 3, 1 jokerul
+### i = 6
+``` 
+lungime_sir_curent = 2
+joker = 0
+maxim = 3
+```
+         0  1  2  3  4  5   6  7   8   9 
+    a = [0, 1, 3, 5, 8, 9, 10, 11, 12, 13]
+                     |     |
+                     u     i
+            consecutive? DA
+                lungime_sir_curent = 3
+                i++
 
 
-2).
+...
 
-        ======          
-        ue  ls
-        ======
-        2   1 
-        4   1  \
-        7   2  /   
 
-        ======          
-        ue  ls
-        ======                jokerul
-        2   1                   \/  
-    ->  4   1   \  7 - 2 == 4 + 1 ?
-    ->  7   2<- /   
-     
-        ======          
-        ue  ls
-        ======
-        2   1
-        4   1<-  \  7 - 2 == 4 + 1 DA
-        7   2<-  /  maxim =  1 + 2 + 1, 1 jokerul
+### i = 9
+``` 
+lungime_sir_curent = 5
+joker = 0
+maxim = 5
+```
+         0  1  2  3  4  5   6  7   8   9 
+    a = [0, 1, 3, 5, 8, 9, 10, 11, 12, 13]
+                     |                  |
+                     u                  i
+            consecutive? DA
+                lungime_sir_curent = 6
+                maxim = 6
+                i++
+
+### i = 10
+``` 
+lungime_sir_curent = 5
+joker = 0
+maxim = 5
+```
+*** while se opreste ***
+
+    joker neutilizat &&
+     n permite (pentru [8, 15]) DA
+        lungime_sir_curent = 7
+        maxim = 7
+
+### maxim = 7
--- joker/joker.py
+++ joker/joker.py
@@ -6,7 +6,7 @@ def citeste_din_fisier():
         lines = f.readlines()
         n, k = int(lines[0].split()[0]), int(lines[0].split()[1])
         a = []
-        for i in xrange(1, k + 1):
+        for i in range(1, k + 1):
             a.append(int(lines[i]))
         return n, k, a
 
@@ -17,80 +17,76 @@ def main():
     # sorteaza a
     a = sorted(a)
 
-    # vector ce va tine ultimul element al secventelor consecutive
-    # (Ultimul Element)
-    ue = []
-    # vector ce va tine lungimile sirurilor cu capetele din ue (Lungime Sir)
-    ls = []
-
-    if a[0] == 0:
-
-        # cautam capetele secventelor consecutive din a si completam ue si ls
-        # in acelasi timp calculam si maximul din ue.
-        # pentru asta vom avea nevoie de elementul precedent si de lungimea
-        # curenta a sirului
-        lc = 1
-        prec = a[1]
-        maxim = 0
-        index_ue = 0
-        # for(i=2, i<k; i++)
-        for index in xrange(2, k):
-            if a[index] == prec + 1:
-                lc += 1
+    # vom parcurge sirul numerelor sortate si vom retine in u pozitia de
+    # inceput al ultimului sir consecutiv intalnit si vom numara in acelasi
+    # timp si elementele acestuia
+    if a[0] != 0:
+        inceput = 1
+        u = 0
+        exista_joker = 0
+    else:
+        inceput = 2
+        u = 1
+        exista_joker = 1
+    print(a)
+    joker = 0
+    lungime_sir_curent = 1
+    maxim = 1
+    i = inceput
+    while i < k:
+        if a[u] + lungime_sir_curent == a[i]:
+            # daca elementul curent (a[i]) este element
+            # al subsirului consecutiv ce incepe cu a[u]
+            # atunci in numaram
+            lungime_sir_curent += 1
+        elif (exista_joker == 1 and joker == 0 and a[i] - 1 < n and
+              a[u] + lungime_sir_curent + 1 == a[i]):
+                # daca exista 0 in sirul de numere &&
+                # daca jokerul nu a fost deja folosit &&
+                # daca n permite utilizarea jokerului &&
+                # daca se potriveste intre cele 2 subsiruri
+                # (a[u] + lungime_sir_curent + 1 == a[i])
+                lungime_sir_curent += 2
+                # am folosit jokerul
+                joker = 1
+        else:
+            # am ajuns la sfarsitul unui sir crescator unde a[i] nu mai
+            # apartine acestuia
+            if exista_joker == 1 and joker == 0:
+                # exista posibilitatea ca jockerul sa existe si
+                # sa nu se fi folosit pana acum, deci putem sa il
+                # lipim la sirul ce abia s-a terminat, in fata
+                # sau in spate
+                if a[u] - 1 < n or a[i - 1] + 1 < n:
+                    lungime_sir_curent += 1
+                    if lungime_sir_curent > maxim:
+                        # daca nu facem aceasta verificare acum,
+                        # lungime_sir_curent se va pierde (devine 1)
+                        maxim = lungime_sir_curent
+            if exista_joker and joker == 1:
+                u = i - 1
+                # i trebuie sa ramana acelasi, sa compare noile subsiruri
+                # i - 1 anuleaza i + 1
+                i = i - 1
             else:
-                if lc > maxim:
-                    maxim = lc
-                # ue[index_ue] = prec
-                ue.append(prec)
-                # ls[index_ue] = lc
-                ls.append(lc)
-                index_ue += 1
-                lc = 1
-            prec = a[index]
-        # dupa ce for-ul se termina, ultimul element nu va fi adaugat pt ue in
-        # for adaugam elementul prec, nu elementul curent. pentru asta va
-        # trebui sa adaugam de mana ultimul element in ue si ls
-        # ue[index_ue] = prec
-        ue.append(prec)
-        # ls[index_ue] = lc
-        ls.append(lc)
-        if lc > maxim:
-            maxim = lc
+                u = i
+            lungime_sir_curent = 1
+            joker = 0
 
-        # verificam daca exista 2 subsiruri consecutive care impreuna cu
-        # jokerul sa formeze un subsir consecutiv mai lung decat cel care mai
-        # lung subsir consecutiv gasit mai sus
+        if lungime_sir_curent > maxim:
+            maxim = lungime_sir_curent
 
-        # for(i=1, i<index_ue; i++)
-        for index in xrange(1, index_ue):
-            # verificam daca posibilul joker ar fi < n, adica daca avem a si b
-            # 2 subsiruri, ultimul element al lui a sa fie mai mic decat n
-            if (n > ue[index - 1]):
-                # verificam daca diferenta dintre sfarsitul primului subsir si
-                # inceputul al 2 lea subsir este de 2
-                if ue[index] - ls[index] == ue[index - 1] + 1:
-                    # calculam lungimea noului subsir format din cele 2+joker
-                    lungime_sir = ls[index] + ls[index - 1] + 1
-                    if maxim < lungime_sir:
-                        maxim = lungime_sir
-    elif a[0] != 0:
-        # aici doar cautam subsirul consecutiv de lungime maxima
-        lc = 1
-        prec = a[0]
-        maxim = 0
-        # for(i=1, i<k; i++)
-        for index in xrange(1, k):
-            if a[index] == prec + 1:
-                lc += 1
-            else:
-                if lc > maxim:
-                    maxim = lc
-                lc = 1
-            prec = a[index]
-        if lc > maxim:
-            maxim = lc
+        i += 1
 
-    # cout<<maxim
+    if exista_joker == 1 and joker == 0:
+        # daca vectorul sortat se termina cu un si crescator, verificarea
+        # de la linia 54 nu mai are loc, asa ca trebuie sa o facem dupa ce
+        # for-ul se termina
+        if a[i - 1] + 1 < n or a[u] - 1 < n:
+            lungime_sir_curent += 1
+            if lungime_sir_curent > maxim:
+                maxim = lungime_sir_curent
+
     print(maxim)
 
 
