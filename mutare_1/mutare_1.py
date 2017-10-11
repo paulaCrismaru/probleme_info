42b18e16a085429acaffd2fcf074efa1bd9d1625
Paula Crismaru <paulacrismaru@gmail.com>
2017-10-11 10:54:37 +0300

    Fix mutare_1

--- mutare_1/mutare_1.py
+++ mutare_1/mutare_1.py
@@ -1,25 +1,33 @@
-def mutare(n, v, start):
-    index_impar = index_par = -1
-    for i in xrange(start, n):
-        if v[i] % 2 == 0 and index_par == -1:
-                index_par = i
-        elif v[i] % 2 != 0 and index_impar == -1:
-            if index_impar == -1:
-                index_impar = i
-        elif index_par != -1 and index_impar != -1:
-            break
-    if index_impar != -1 and index_par != -1:
-        aux = v[index_par]
-        v[index_par] = v[index_impar]
-        v[index_impar] = aux
-        return True, index_par + 1
-    return False, index_par + 1
-
-
 def aranjare(v, n):
     start = 0
+    sch = False
     while True:
-        sch, start = mutare(n, v, start)
+        index_impar = index_par = -1
+        for i in xrange(start, n):
+            if v[i] % 2 == 0 and index_par == -1:
+                    index_par = i
+            elif v[i] % 2 != 0 and index_impar == -1:
+                if index_impar == -1:
+                    index_impar = i
+            elif index_par != -1 and index_impar != -1:
+                break
+        if index_impar != -1 and index_par != -1:
+            if index_par < index_impar:
+                # numarul par este in stanga numarului impar
+                # deci le schimbam intre ele
+                aux = v[index_par]
+                v[index_par] = v[index_impar]
+                v[index_impar] = aux
+                sch = True
+            else:
+                # numarul par este in dreapta numarului impar
+                # deci ordinea este corecta. trecem la urmatorul
+                # numar, adica cel din dreapta numarului impar
+                start = index_impar + 1
+                sch = True
+        else:
+            # sunt numai numere impare sau numai pare
+            break
         if not sch:
             break
 
@@ -29,6 +37,7 @@ def main():
     v = [22, 38, 42, 0, 0, 0, 8]
     v = [1, 3, 5, 7, 9, 19, 29]
     v = [22, 3, 5, 2, 9, 19, 29]
+    v = [21, 37, 42, 7, 48, 25, 9]
     n = 7
     aranjare(v, n)
     print v
